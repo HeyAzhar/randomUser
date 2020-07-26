573758a8bcfc4995c52870319db00c2e2ac9b803
HeyAzhar <sk.azharuddin1230@gmail.com>
2020-07-27 04:16:24 +0530

    Updates

---
+++ precache-manifest.4c597e13dd1ceea213549ab36f072194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159ee3343fcf3b9edba4dc4e59af56e0",
+    "url": "/randomUser/index.html"
+  },
+  {
+    "revision": "08c8a07b1b2d57b3345a",
+    "url": "/randomUser/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc682d00d54ec38249cb",
+    "url": "/randomUser/static/css/main.ee95caa9.chunk.css"
+  },
+  {
+    "revision": "08c8a07b1b2d57b3345a",
+    "url": "/randomUser/static/js/2.68e26e53.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/randomUser/static/js/2.68e26e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc682d00d54ec38249cb",
+    "url": "/randomUser/static/js/main.91f3d1b1.chunk.js"
+  },
+  {
+    "revision": "a5fd940016d219f9e955",
+    "url": "/randomUser/static/js/runtime-main.cc35f46a.js"
+  }
+]);
